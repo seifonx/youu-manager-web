3a6db96a318f4ee411351e813bea4ab9aa3fe087
ThrumanOne <1006556989@qq.com>
2017-08-10 09:48:55 +0800

    shiro再次整合

---
+++ src/main/java/com/youu/youu/manager/shiro/FilterChainDefinitionMapBuilder.java
@@ -0,0 +1,21 @@
+package com.youu.youu.manager.shiro;
+
+import java.util.LinkedHashMap;
+
+public class FilterChainDefinitionMapBuilder {
+	public LinkedHashMap<String, String> buildFilterChainDefinitionMap(){
+		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+		
+		map.put("/login.jsp", "anon");
+		map.put("/index.jsp", "anon");
+		map.put("/shiro/login", "anon");
+		map.put("/shiro/logout", "logout");
+		map.put("/user.jsp", "authc,roles[user]");
+		map.put("/admin.jsp", "authc,roles[admin]");
+		map.put("/list.jsp", "user");
+		
+		map.put("/**", "authc");
+		
+		return map;
+	}
+}
